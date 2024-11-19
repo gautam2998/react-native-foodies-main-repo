82103becc9a8ccca5c434d30016b88db62242533
gautam.pandey.0407@gmail.com <gautam.pandey.0407@gmail.com>
2024-11-19 18:07:15 +0530

    new-commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  plugins: ['import'],
+  rules: {
+    'no-console': 'error',
+  },
+  // settings: {
+  //   'import/resolver': {
+  //     node: {
+  //       paths: ['src'],
+  //       alias: {
+  //         components: './src/components',
+  //         common: './src/common',
+  //         theme: './src/theme',
+  //         src: './src',
+  //         '@navigation': './src/navigation',
+  //         '@components': './src/components',
+  //         '@assets': './assets',
+  //         '@api': './src/api',
+  //         '@model': './src/model',
+  //         '@common': './src/common',
+  //         '@redux': './src/redux',
+  //        
+  //       },
+  //     },
+  //   },
+  // },
+};
